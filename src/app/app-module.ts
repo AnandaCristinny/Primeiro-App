3775c55a20f280eb022d51c493d43947b466aa52
Ananda <anandacristinny@gmail.com>
2025-06-24 13:35:52 -0300

    Primeiro-App

---
+++ src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { ExibeMensagem } from './exibe-mensagem/exibe-mensagem';
+
+@NgModule({
+  declarations: [
+    App,
+    ExibeMensagem
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
